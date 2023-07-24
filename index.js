d7a778cdde48695c30042e876f72d1aaa84cd7eb
ZUBAIR SHEIKH <zubaireditz4U@gmail.com>
2023-07-24 12:12:39 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function toggleForm() {
+    var loginForm = document.getElementById('login-form');
+    var signupForm = document.getElementById('signup-form');
+
+    loginForm.classList.toggle('show');
+    signupForm.classList.toggle('show');
+  }
